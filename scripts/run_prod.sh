8129de74b7d09c622e9825417657e4ca83865813
Vasylchenko <vasylchenko@pixelfield.cz>
2023-09-06 17:43:19 +0200

    Fixed aerich deploy

--- scripts/run_prod.sh
+++ scripts/run_prod.sh
@@ -1,5 +1,6 @@
 #! /usr/bin/bash
 
 pybabel compile -d translations/locales
+python -c 'from migrations import utils; utils.command.upgrade_zero_sync()'
 aerich upgrade
 uvicorn main:app --host ${HOST:-0.0.0.0} --port ${PORT:-8000} --proxy-headers --forwarded-allow-ips='*'
